3823d9d05f39549fec3d3faf4e371d31ae731341
hotspot23 <146493453+hotspot23@users.noreply.github.com>
2024-09-04 21:09:41 -0400

    Update build_all_and_publish_dockerhub.sh

--- build_all_and_publish_dockerhub.sh
+++ build_all_and_publish_dockerhub.sh
@@ -5,20 +5,20 @@ echo "dockerhub\`s username: $1"
 cd shopfront
 mvn clean install -DskipTests=true
 if docker build -t $1/djshopfront . ; then
-  docker push shegerbootcamp/djshopfront
+  docker push amen2016/djshopfront
 fi
 cd ..
 
 cd productcatalogue
 mvn clean install -DskipTests=true
 if docker build -t $1/djproductcatalogue . ; then
-  docker push shegerbootcamp/djproductcatalogue
+  docker push amen2016/djproductcatalogue
 fi
 cd ..
 
 cd stockmanager
 mvn clean install -DskipTests=true
 if docker build -t $1/djstockmanager . ; then
-  docker push shegerbootcamp/djstockmanager
+  docker push amen2016/djstockmanager
 fi
 cd ..
